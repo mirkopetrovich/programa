dda78495b75a3a677c81f35f22bf2736dc6b476a
mirkopetrovich <mirko.petrovich@gmail.com>
2024-10-23 18:07:19 -0300

    2 fichas

--- fichas/lee_fichas_2/lee_fichas_2.ino
+++ fichas/lee_fichas_2/lee_fichas_2.ino
@@ -46,53 +46,32 @@ byte ssPins[] = {SS_1_PIN, SS_2_PIN};
 
 MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
 
-/**
- * Initialize.
- */
 void setup() {
 
-  //Serial.begin(9600); // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-
   SPI.begin();        // Init SPI bus
   matrix.begin();
 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
-   /* Serial.print(F("Reader "));
-    Serial.print(reader);
-    Serial.print(F(": "));
-    mfrc522[reader].PCD_DumpVersionToSerial();*/
   }
 }
 
-/**
- * Main loop.
- */
 void loop() {
 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     // Look for new cards
-
     if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
-      /*Serial.print(F("Reader "));
-      Serial.print(reader);
-      // Show some details of the PICC (that is: the tag/card)
-      Serial.print(F(": Card UID:"));
-      dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
-      Serial.println();
-      Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
-      Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));*/
-      ficha_led(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+      
+      if (reader==0) ficha_led_1(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
 
-
+      if (reader==1) ficha_led_2(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
       // Halt PICC
      // mfrc522[reader].PICC_HaltA();
       // Stop encryption on PCD
       mfrc522[reader].PCD_StopCrypto1();
-    } //if (mfrc522[reader].PICC_IsNewC
-  } //for(uint8_t reader
+    } 
+  } 
 }
 
 /**
@@ -122,6 +101,50 @@ void ficha_led(byte *buffer, byte bufferSize) {
   matrix.endText();
   matrix.endDraw();
   delay (1000);
+  matrix.beginDraw();
+   matrix.stroke(0xFFFFFFFF);
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println("  ");
+  matrix.endText();
+  matrix.endDraw();
+
+  
+}
+
+void ficha_led_1(byte *buffer, byte bufferSize) {
+
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.print(buffer[bufferSize-1] < 0x10 ? "0":"");
+  matrix.println(buffer[bufferSize-1],HEX);
+  matrix.endText();
+  matrix.endDraw();
+  delay (100);
+  matrix.beginDraw();
+   matrix.stroke(0xFFFFFFFF);
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println("  ");
+  matrix.endText();
+  matrix.endDraw();
+
+  
+}
+
+void ficha_led_2(byte *buffer, byte bufferSize) {
+
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.print(buffer[bufferSize-1] < 0x10 ? "  0":"  ");
+  matrix.println(buffer[bufferSize-1],HEX);
+  matrix.endText();
+  matrix.endDraw();
+  delay (100);
   matrix.beginDraw();
    matrix.stroke(0xFFFFFFFF);
   matrix.textFont(Font_4x6);
